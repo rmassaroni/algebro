1f42c5741b1ad99268fd53a19a598e7f01974203
rjm <robjmass@gmail.com>
2024-02-29 22:31:49 -0500

    coef param

--- main/variable.cpp
+++ main/variable.cpp
@@ -4,13 +4,14 @@ using namespace std;
 
 class Variable {
 public:
-    Variable(string name = "", int value = 1);
+    Variable(string name = "", int value = 1, int coefficient = 1);
 private:
     string name;
     int value;
+    int coefficient;
 };
 
-Variable::Variable(string name, int value) {
+Variable::Variable(string name, int value, int coefficient) {
     this->name = name;
     this->value = value;
 }
