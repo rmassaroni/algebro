ac5c9ff4e2038f31433634a860ec8566ca894ac8
rjm <robjmass@gmail.com>
2024-03-19 16:12:40 -0400

    setCoef

--- kot/Variable.kt
+++ kot/Variable.kt
@@ -1,8 +1,19 @@
 package kot
 
 public class Variable(var name: String = "", var coef: Int = 1) {
+    init {
+        printInfo()
+    }
+
     fun printInfo() {
         println(name)
         println(coef)
+        println(setCoef(name))
+    }
+    
+    fun setCoef(input: String): Int {
+        val regex = Regex("""^\d+""")
+        val matchResult = regex.find(input)
+        return matchResult?.value?.toIntOrNull() ?: 1
     }
 }
