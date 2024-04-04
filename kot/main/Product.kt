add2709b21d6ee0e2d9632de97d18940c968e8bc
rjm <robjmass@gmail.com>
2024-04-03 20:49:36 -0400

    product stuff

--- kot/main/Product.kt
+++ kot/main/Product.kt
@@ -2,6 +2,18 @@ package main
 
 import main.Variable
 
-public class Product(var vars: ArrayList<Variable> = ArrayList()) {
-    
+public class Product(var name: String = "", var vars: ArrayList<Variable> = ArrayList()) {
+    //is name neccesary?
+
+    init {
+        println("Product of: " + vars.toString())
+        println("Terms: " + getTerms().toString())
+    }
+
+    public fun getTerms(): ArrayList<String> {
+        var terms: ArrayList<String> = ArrayList()
+        for(v in vars)
+            if(terms.contains(v.term) == false) terms.add(v.term)
+        return terms
+    }
 }
